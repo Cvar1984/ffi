f8c18892f09436a855a9000f4d4631936a8570a7
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-07-20 21:40:22 +0800

    initial commit

--- .ccls-cache/@data@data@com.termux@files@home@ffi/include@libcurl.h
+++
@@ -1,7 +0,0 @@
-#define FFI_SCOPE "LIBCURL"
-#define FFI_LIB "libcurl.so"
-
-int *curl_easy_init();
-int curl_easy_setopt(int *ch, int const CURLOPT, ...);
-int curl_easy_perform(int *ch);
-void curl_easy_cleanup(int *ch);
--- .ccls-cache/@data@data@com.termux@files@home@ffi/include@test.h
+++
@@ -1,4 +0,0 @@
-#define FFI_SCOPE "TEST"
-#define FFI_LIB "libc.so"
-
-void printf(char const *txt, ...);
